2d85e0bf949b4b2ac278788c624d09458fe37fdb
Iashin Dmytro <iashin.d@nixsolutions.com>
2017-02-13 22:02:39 +0200

    implement login action

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AuthService} from "../../shared/services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  login: string = '';
+  password: string = '';
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit() {
+    this.auth.signIn(this.login, this.password)
+      .then(() => this.router.navigate(['']));
   }
 
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -2,31 +2,50 @@ import { Injectable } from '@angular/core';
 import {ApiService} from "../api/api.service";
 import {CanActivate, Router} from "@angular/router";
 import * as Promise from "../../../../../../../../Library/Preferences/WebStorm2016.3/javascript/extLibs/http_github.com_DefinitelyTyped_DefinitelyTyped_raw_master_promise_index";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import {toPromise} from "rxjs/operator/toPromise";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AuthService implements CanActivate{
+
+  loggedIn: boolean = false;
 
   constructor(
     private api: ApiService,
     private router: Router
   ) { }
 
-  canActivate(): Promise<boolean> | boolean {
+  signIn(login, password): Promise<any> {
+    return this.api.post('/login', {login, password })
+      .map(response => {
+        if (response.token) {
+          this.api.token = response.token;
+          this.loggedIn = true;
+          return response;
+        }
+      }).toPromise();
+  }
+
+  canActivate(): Promise<boolean> | Observable<boolean>| boolean {
 
     if (!this.api.token) {
       this.router.navigate(['auth']);
       return false;
     }
 
-    return this.api.get('/me', {me: 'me', second: 2})
-      .map(response => response.json())
-      .map(payload => !!payload.loggedIn)
+    if (this.loggedIn) {
+      return true;
+    }
+
+    return this.api.get('/api/me')
+      .map(payload => payload.loggedIn)
       .map(loggedIn => {
         if (!loggedIn) {
           this.router.navigate(['auth']);
         }
         return loggedIn;
-      })
-      .toPromise();
+      });
   }
 }
